ad06268ae402d8cc55bd837e3f8721aef364636e
john lopez <johnguitarmusica@gmail.com>
2023-01-09 22:27:52 -0500

    agregams y entendimos variables de entorn con webpack y alias de webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,72 +2,71 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
-
-
-
-
+const Dotenv = require("dotenv-webpack");
 module.exports = {
   entry: "./src/index.js",
-  output:{
-    path: path.resolve(__dirname, "dist" ),
-    filename: "main.js"  ,
-    assetModuleFilename:" assets/images/[hash][ext][query]"
-},
-resolve:{
-    extensions:[".js"]
-},
-module:{
-  rules: [ 
-    {
-    test: /\.m?js$/,
-    exclude: /node_modules/,
-    use:{
-      loader : "babel-loader"
-    }
-   },
-   {
-    test: /\.css|.styl$/i,
-    use: [MiniCssExtractPlugin.loader,
-    "css-loader",
-    "stylus-loader"
-    ],
-   },
-   {
-    test:/\.png/,
-    type: "asset/resource"
-   },
-   {
-    test: /\.(woff|woff2)$/,
-    use:{
-      loader:"url-loader",
-      options:{
-        limit: 10000,
-        mimetype:"application/font-woff",
-        name:"[name].[ext]",
-        outputPath: "./assets/fonts/",
-        publicPath: "./assets/fonts/",
-        esModule:false,          
-        },
-      }
-   }
-
-  ]
-  
-},
- plugins:[
-  new HtmlWebpackPlugin({
-    inject:true,
-    template: "./public/index.html",
-    filename: "./index.html"
-  }),
-  new MiniCssExtractPlugin(),
-  new CopyPlugin({
-    patterns:[
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    assetModuleFilename: " assets/images/[hash][ext][query]",
+  },
+  resolve: {
+    extensions: [".js"],
+    alias: {
+      "@utils": path.resolve(__dirname, "src/utils/"),
+      "@templates": path.resolve(__dirname, "src/templates/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+      "@images": path.resolve(__dirname, "src/assets/images/"),
+    },
+  },
+  module: {
+    rules: [
       {
-      from: path.resolve(__dirname, "src", "assets/images"),
-      to: "assets/images"
-      }
-    ]
-  })
- ]
-}
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css|.styl$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
+      },
+      {
+        test: /\.png/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimetype: "application/font-woff",
+            name: "[name].[ext]",
+            outputPath: "./assets/fonts/",
+            publicPath: "../assets/fonts/",
+            esModule: false,
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images",
+        },
+      ],
+    }),
+    new Dotenv(),
+  ],
+};
